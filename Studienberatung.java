50c2c9d8728dcf7a54e08b7ab0d7d870bdeafc8d
Christoph- <christophmennerich@yahoo.de>
2015-05-06 14:53:26 +0200

    added hast du fachabitur nein zweig

--- Studienberatung.java
+++ Studienberatung.java
@@ -5,5 +5,12 @@
 
 		System.out.println(„Hast du Abitur?“);
 		String answer = scanner.next();
+
+		System.out.println(„Hast du Fachabitur?“)
+		answer = scanner.next();
+		if(answer.equalsIgnoreCase(„n“){
+		// Code für kein Abitur
+		}
+		
 		}
 	}
