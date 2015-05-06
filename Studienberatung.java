4e48383472c1ab822fdb97c6400baa82400dfe5b
Christoph- <christophmennerich@yahoo.de>
2015-05-06 15:13:11 +0200

    added bist du wissbegierig ja zweig

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,7 +3,14 @@
 		public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println(„Hast du Abitur?“);
+		System.out.println("Hast du Abitur?");
 		String answer = scanner.next();
+
+		System.out.println("Bist du wissbegierig?")
+		answer = scanner.next();
+		if(answer.equalsIgnoreCase(„j“){
+		// Code für Abitur
+		}
+
 		}
 	}
